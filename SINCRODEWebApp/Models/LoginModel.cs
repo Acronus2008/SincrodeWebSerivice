d6e645c4ba90f9cc1d30e4304d8ec483dbf386d4
Alejandro <acronus2008@gmail.com>
2020-01-09 18:30:05 -0800

    adding loggin model

---
+++ SINCRODEWebApp/Models/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SINCRODEWebApp.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage ="El usuario es requerido")]
+        [Display(Name ="Usuario")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage ="La contraseña es requerida")]
+        [Display(Name ="Contraseña")]
+        public string Password { get; set; }
+    }
+}
