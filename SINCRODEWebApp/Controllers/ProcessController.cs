121c855d6e2642098fe28e431518bebb65de9c02
Alejandro <acronus2008@gmail.com>
2020-01-09 18:30:17 -0800

    secured process controller index

--- SINCRODEWebApp/Controllers/ProcessController.cs
+++ SINCRODEWebApp/Controllers/ProcessController.cs
@@ -5,7 +5,12 @@ namespace SINCRODEWebApp.Controllers
     public class ProcessController : Controller
     {
         public IActionResult Index()
-        {  
+        {
+            if (!HttpContext.User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
             return View();
         }
 
