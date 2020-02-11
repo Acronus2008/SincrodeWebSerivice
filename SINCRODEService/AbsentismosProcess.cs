564ded5759994b183d59574a7d2a25b6a8d8070e
Alejandro <acronus2008@gmail.com>
2020-02-11 08:40:45 -0800

    expose absentismos class

---
+++ SINCRODEService/AbsentismosProcess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SINCRODEService
+{
+    public static class AbsentismosProcess
+    {
+        public static bool ExecuteAbsentismosProcess(DateTime fechaini, DateTime fechafin, bool AutoPro = true)
+        {
+            try
+            {
+                SincroDEService.ProcesaAusencias(fechaini, fechafin, AutoPro);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
