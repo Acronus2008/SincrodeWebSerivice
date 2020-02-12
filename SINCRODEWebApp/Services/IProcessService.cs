6276dafafc0de9afcfbd7d05c3994cdd4df6b5cd
Alejandro <acronus2008@gmail.com>
2020-02-12 06:04:41 -0800

    adding interface for process

---
+++ SINCRODEWebApp/Services/IProcessService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using SINCRODEWeb.Models;
+using SINCRODEWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace SINCRODEWebApp.Services
+{
+    public interface IProcessService
+    {
+        List<ProcessModel> GetProcess(string searchCriteria);
+
+        List<ProcessModel> ProcessCollection(List<ProcessModel> lstElements, IFormCollection requestFormData);
+
+        List<LogsModel> ProcessCollection(List<LogsModel> lstElements, IFormCollection requestFormData);
+
+    }
+}
