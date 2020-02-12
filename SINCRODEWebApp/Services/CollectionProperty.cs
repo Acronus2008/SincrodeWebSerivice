7ee161c1e4e78b37a77453862da6af71c6f88066
Alejandro <acronus2008@gmail.com>
2020-02-12 06:05:13 -0800

    encapsulate methods for process colections

---
+++ SINCRODEWebApp/Services/CollectionProperty.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using SINCRODEWeb.Models;
+using SINCRODEWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace SINCRODEWebApp.Services
+{
+    public abstract class CollectionProperty
+    {
+        public PropertyInfo getProperty(string name)
+        {
+            var properties = typeof(ProcessModel).GetProperties();
+            PropertyInfo prop = null;
+            foreach (var item in properties)
+            {
+                if (item.Name.ToLower().Equals(name.ToLower()))
+                {
+                    prop = item;
+                    break;
+                }
+            }
+            return prop;
+        }
+
+        public List<ProcessModel> BuildCollectionFromListOfElement(List<ProcessModel> lstElements, IFormCollection requestFormData)
+        {
+            var skip = Convert.ToInt32(requestFormData["start"].ToString());
+            var pageSize = Convert.ToInt32(requestFormData["length"].ToString());
+            Microsoft.Extensions.Primitives.StringValues tempOrder = new[] { "" };
+
+            if (requestFormData.TryGetValue("order[0][column]", out tempOrder))
+            {
+                var columnIndex = requestFormData["order[0][column]"].ToString();
+                var sortDirection = requestFormData["order[0][dir]"].ToString();
+                tempOrder = new[] { "" };
+                if (requestFormData.TryGetValue($"columns[{columnIndex}][data]", out tempOrder))
+                {
+                    var columName = requestFormData[$"columns[{columnIndex}][data]"].ToString();
+
+                    if (pageSize > 0)
+                    {
+                        var prop = getProperty(columName);
+                        if (sortDirection == "asc")
+                        {
+                            return lstElements.OrderBy(prop.GetValue).Skip(skip).Take(pageSize).ToList();
+                        }
+
+                        return lstElements.OrderByDescending(prop.GetValue).Skip(skip).Take(pageSize).ToList();
+                    }
+
+                    return lstElements;
+                }
+            }
+            return null;
+        }
+
+        public List<LogsModel> BuildCollectionFromListOfElement(List<LogsModel> lstElements, IFormCollection requestFormData)
+        {
+            var skip = Convert.ToInt32(requestFormData["start"].ToString());
+            var pageSize = Convert.ToInt32(requestFormData["length"].ToString());
+            Microsoft.Extensions.Primitives.StringValues tempOrder = new[] { "" };
+
+            if (requestFormData.TryGetValue("order[0][column]", out tempOrder))
+            {
+                var columnIndex = requestFormData["order[0][column]"].ToString();
+                var sortDirection = requestFormData["order[0][dir]"].ToString();
+                tempOrder = new[] { "" };
+                if (requestFormData.TryGetValue($"columns[{columnIndex}][data]", out tempOrder))
+                {
+                    var columName = requestFormData[$"columns[{columnIndex}][data]"].ToString();
+
+                    if (pageSize > 0)
+                    {
+                        var prop = getProperty(columName);
+                        if (sortDirection == "asc")
+                        {
+                            return lstElements.OrderBy(prop.GetValue).Skip(skip).Take(pageSize).ToList();
+                        }
+
+                        return lstElements.OrderByDescending(prop.GetValue).Skip(skip).Take(pageSize).ToList();
+                    }
+
+                    return lstElements;
+                }
+            }
+            return null;
+        }
+    }
+}
